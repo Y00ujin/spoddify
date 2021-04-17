00a308cb02d2d1a70bf6ab440ed885ae74059b14
Y00ujin <yujingim43@gmail.com>
2021-04-17 23:49:44 +0900

    [Modify] HomeViewController PlaylistCollectionView Cell Resize

--- spoddify/ViewController/HomeViewController.swift
+++ spoddify/ViewController/HomeViewController.swift
@@ -11,6 +11,8 @@ class HomeViewController: UIViewController{
     @IBOutlet weak var settingButton: UIButton!
     @IBOutlet weak var playlistCollectionView: UICollectionView!
     
+    let sectionInsets = UIEdgeInsets(top: 10, left: 15, bottom: 10, right: 15)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -20,7 +22,7 @@ class HomeViewController: UIViewController{
     
 }
 
-extension HomeViewController: UICollectionViewDelegate, UICollectionViewDataSource{
+extension HomeViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout{
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 6
     }
@@ -31,5 +33,30 @@ extension HomeViewController: UICollectionViewDelegate, UICollectionViewDataSour
         return cell
     }
     
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = collectionView.frame.width
+        let height = collectionView.frame.height
+        
+        let itemsPerRow: CGFloat = 2
+        let widthPadding = sectionInsets.left * (itemsPerRow + 1)
+        let itemsPerColumn: CGFloat = 3
+        let heightPadding = sectionInsets.top * (itemsPerColumn + 1)
+        
+        let cellWidth = (width - widthPadding) / itemsPerRow
+        let cellHeight = (height - heightPadding) / itemsPerColumn
+            
+        return CGSize(width: cellWidth, height: cellHeight)
+    }
     
+    func collectionView(_ collectionView: UICollectionView,
+                            layout collectionViewLayout: UICollectionViewLayout,
+                            insetForSectionAt section: Int) -> UIEdgeInsets {
+        return sectionInsets
+    }
+        
+    func collectionView(_ collectionView: UICollectionView,
+                            layout collectionViewLayout: UICollectionViewLayout,
+                        minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return sectionInsets.left
+    }
 }
