b8f3c97ca9c1fa6b431b1ff7659b3537cd53b25a
Y00ujin <yujingim43@gmail.com>
2021-04-19 17:35:13 +0900

    [Completed] SettingViewController TableView Completed

---
+++ spoddify/TableViewCell/SettingTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  SettingTableViewCell.swift
+//  spoddify
+//
+//  Created by 김유진 on 2021/04/19.
+//
+
+import UIKit
+
+class SettingTableViewCell: UITableViewCell{
+    @IBOutlet weak var settingLabel: UILabel!
+    @IBOutlet weak var settingButton: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
---
+++ spoddify/ViewController/SettingViewController.swift
@@ -0,0 +1,42 @@
+//
+//  settingViewController.swift
+//  spoddify
+//
+//  Created by 김유진 on 2021/04/19.
+//
+
+import UIKit
+
+class SettingViewController: UIViewController{
+    @IBOutlet weak var profileButton: UIButton!
+    @IBOutlet weak var topBarView: UIView!
+    @IBOutlet weak var settingTableView: UITableView!
+    
+    var settingLabel = ["데이터 세이버", "계정", "재생하기","유해성 콘텐츠","디바이스","자동차","공개 범위","앱에 연결하기","오디오 품질","저장 공간","알림","광고", "저장 파일", "상세정보"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        settingTableView.delegate = self
+        settingTableView.dataSource = self
+    }
+    @IBAction func profileButton(_ sender: Any) {
+        print("profileButton cliked")
+        profileButton.layer.backgroundColor = UIColor.black.cgColor
+    }
+}
+
+extension SettingViewController: UITableViewDelegate, UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return settingLabel.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "SettingTableViewCell", for: indexPath) as! SettingTableViewCell
+        
+        cell.settingLabel?.text = settingLabel[indexPath.row]
+        
+        return cell
+    }
+}
+
